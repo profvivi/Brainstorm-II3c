bbf386e547883fd6360cbea35bcc7cd6f7765df7
Viviane <106716101+profvivi@users.noreply.github.com>
2026-09-23 14:32:12 -0300

    s

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    
+    botaoJogarNovamente.addEventListener("click". jogaNovamente());   
 }
 //incluir aula5
 function jogaNovamente() {
