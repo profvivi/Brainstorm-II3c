417c153a0762e39147dc1159cd3e49c7b9a9aa3c
Viviane <106716101+profvivi@users.noreply.github.com>
2026-09-23 16:26:50 -0300

    s

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente= document querySelector(".novamente.btn");
+const botaoJogarNovamente= document.querySelector(".novamente-btn");  //aula4
 
 let atual = 0; 
 let perguntaAtual;
@@ -44,16 +44,17 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    botaoJogarNovamente.addEventListener("click". jogaNovamente());   
+    botaoJogarNovamente.addEventListener("click", jogaNovamente());   //aula4
 }
-//incluir aula5
+
+//incluir aula4
 function jogaNovamente() {
-    stual= "0"
+    atual= "0"
     historiaFinal = "";
     mostraPergunta();
 }
 
-//INCLUIR aleatorio
+//retirado aleatorio
 
 
 mostraPergunta();
